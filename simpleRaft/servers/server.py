c77ab6a217abd1553103cfbfcb22667d91bbafbf
cyni <cyni@umich.edu>
2018-04-16 17:16:29 -0400

    added crypto functionality

--- simpleRaft/servers/server.py
+++ simpleRaft/servers/server.py
@@ -1,5 +1,9 @@
 import zmq
 import threading
+import pickle
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
 
 class Server(object):
@@ -41,11 +45,20 @@ class Server(object):
 
         self._state = state
 
+# private key is private key for server
+# public keys maps _name to public key for that server
+
 
 class ZeroMQServer(Server):
-    def __init__(self, name, state, log, messageBoard, neighbors, port=6666):
-        super(ZeroMQServer, self).__init__(name, state, log, messageBoard, neighbors)
+    def __init__(self, name, state, log, messageBoard, neighbors,
+                 private_key,
+                 public_keys,
+                 port=6666):
+        super(ZeroMQServer, self).__init__(name, state, log,
+                                           messageBoard, neighbors)
         self._port = 6666
+        self.public_keys = public_keys
+        self.private_key = private_key
 
         class SubscribeThread(threading.Thread):
             def run(thread):
@@ -56,6 +69,20 @@ class ZeroMQServer(Server):
 
                 while True:
                     message = socket.recv()
+                    message_encrypted = message.data["encrypted"]
+                    message_signature = message.data["signature"]
+
+                    # Decrypt message data
+                    message_str = self.private_key.decrypt(message_encrypted)
+
+                    # Verify message
+                    public_key = public_keys[message.sender]
+                    message_hash = SHA256.new(message_str).digest()
+                    if not public_key.verify(message_hash, message_signature):
+                        continue
+
+                    # Process recieved message
+                    message.data = pickle.loads(message_str)
                     self.on_message(message)
 
         class PublishThread(threading.Thread):
@@ -67,7 +94,22 @@ class ZeroMQServer(Server):
                 while True:
                     message = self._messageBoard.get_message()
                     if not message:
-                        continue # sleep wait?
+                        continue
+
+                    # Serialize message data
+                    message_str = pickle.dumps(message.data)
+
+                    # Sign message
+                    message_hash = SHA256.new(message_str).digest()   
+                    message_signature = self.private_key.sign(message_hash, '')
+
+                    # Encryped message
+                    public_key = public_keys[message.receiver]
+                    enc_data = public_key.encrypt(message_str, 32)
+
+                    # Send encrypted message
+                    message.data = {"encrypted_message": encrypted_message,
+                                    "signature": message_signature}
                     socket.send(message)
 
         self.subscribeThread = SubscribeThread()
